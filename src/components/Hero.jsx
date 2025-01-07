6eb1b5517f8b4a7f53ade5657b2783c5778ece6c
Shashank Musale <shashankbmusale07@gmail.com>
2025-01-07 18:07:49 +0530

    second commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,8 @@
 const Hero = () => {
   return (
-    <div className="flex items-center flex-col h-[50vh] dark:bg-gray-800">
+    <div className="flex items-center flex-col h-[42em] dark:bg-gray-800">
       <div className="mt-20 px-12 py-4">
-        <h1 className="py-6 text-5xl font-bold text-gray-600 flex justify-center text-center dark:text-gray-400 ">
+        <h1 className="py-6 text-5xl font-bold text-gray-600 flex justify-center text-center dark:text-gray-400">
           Explore the Possibilities <br /> of AI Chatting with Brainwave
         </h1>
         <p className="py-4 text-2xl font-bold flex justify-center text-center dark:text-gray-300">
