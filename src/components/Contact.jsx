74c367e2e5e8b1e597b8befc1f3f385bd853b91e
Shashank <shashankbmusale07@gmail.com>
2025-01-15 13:14:56 +0530

    css of submit btn modified

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -96,6 +96,7 @@ const Contact = () => {
                 id="name"
                 placeholder="Enter your full name"
                 className="w-full mt-1 px-4 py-2 border border-gray-300 dark:border-gray-800 rounded-md focus:ring-2 focus:ring-blue-500 focus:outline-none"
+                required
               />
             </div>
             <div>
@@ -111,6 +112,7 @@ const Contact = () => {
                 id="email"
                 placeholder="Enter your email"
                 className="w-full mt-1 px-4 py-2 border border-gray-300 dark:border-gray-800 rounded-md focus:ring-2 focus:ring-blue-500 focus:outline-none"
+                required
               />
             </div>
             <div>
@@ -126,11 +128,12 @@ const Contact = () => {
                 rows="4"
                 placeholder="Write your message"
                 className="w-full mt-1 px-4 py-2 border border-gray-300 dark:border-gray-800 rounded-md focus:ring-2 focus:ring-blue-500 focus:outline-none"
+                required
               ></textarea>
             </div>
             <button
               type="submit"
-              className="w-full bg-gray-300 dark:bg-gray-700 dark:text-gray-100 text-gray-700 font-bold py-2 px-4 rounded-md hover:bg-gray-100 transition"
+              className="w-full bg-gray-300 dark:bg-gray-600 dark:text-gray-100 text-gray-700 font-bold py-2 px-4 rounded-md hover:bg-gray-100 dark:hover:bg-gray-800 active:bg-gray-200 dark:active:bg-gray-700 transition"
             >
               Send Message
             </button>
