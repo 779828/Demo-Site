e982928e56f0356398d4fbf10ad3df6752d75378
Shashank <shashankbmusale07@gmail.com>
2025-01-18 09:05:03 +0530

    mail section created

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,7 +1,7 @@
 import { useRef } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { addSubmission } from "../features/formData/dataSlice";
-import axios from "axios";
+// import axios from "axios";
 
 const Contact = () => {
   const data = useSelector((state) => state.user?.data);
@@ -21,22 +21,47 @@ const Contact = () => {
     dispatch(addSubmission(formData));
 
     // Send the data to the backend (MongoDB)
+    //   try {
+    //     const response = await axios.post("https://demo-site-api.vercel.app", {
+    //       name,
+    //       email,
+    //       message,
+    //     });
+
+    //     console.log("Server Response:", response.data);
+    //     alert("Form submitted successfully!");
+
+    //     nameRef.current.value = "";
+    //     emailRef.current.value = "";
+    //     messageRef.current.value = "";
+    //   } catch (error) {
+    //     console.error("Error submitting form:", error);
+    //     alert("Failed to submit the form");
+    //   }
+    // };
+
     try {
-      const response = await axios.post("https://demo-site-api.vercel.app", {
-        name,
-        email,
-        message,
-      });
+      const response = await fetch(
+        "https://demo-site-api.vercel.app/api/send-email",
+        {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(formData),
+        }
+      );
 
-      console.log("Server Response:", response.data);
-      alert("Form submitted successfully!");
+      if (response.ok) {
+        alert("Email sent successfully!");
 
-      nameRef.current.value = "";
-      emailRef.current.value = "";
-      messageRef.current.value = "";
+        nameRef.current.value = "";
+        emailRef.current.value = "";
+        messageRef.current.value = "";
+      } else {
+        alert("Failed to send email. Please try again later.");
+      }
     } catch (error) {
-      console.error("Error submitting form:", error);
-      alert("Failed to submit the form");
+      console.error("Error sending email:", error);
+      alert("An error occurred. Please try again.");
     }
   };
 
