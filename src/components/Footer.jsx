5a5b35fa0df65f4d339beba661c4e2e0b3e9f12b
Shashank <shashankbmusale07@gmail.com>
2025-01-22 11:03:35 +0530

    change text color

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
             <h2 className="text-2xl font-bold bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 bg-clip-text text-transparent mb-3">
               Sha-Shank
             </h2>
-            <p className="text-gray-400">
+            <p className="text-gray-300">
               Your Trusted Source for Quality Products! Delivering quality and
               excellence since 2020.
             </p>
@@ -21,7 +21,7 @@ const Footer = () => {
               <li>
                 <a
                   href="/home"
-                  className="text-gray-400 hover:text-white transition"
+                  className="text-gray-300 hover:text-white transition"
                 >
                   Home
                 </a>
@@ -29,7 +29,7 @@ const Footer = () => {
               <li>
                 <a
                   href="/about"
-                  className="text-gray-400 hover:text-white transition"
+                  className="text-gray-300 hover:text-white transition"
                 >
                   About
                 </a>
@@ -37,7 +37,7 @@ const Footer = () => {
               <li>
                 <a
                   href="/contact"
-                  className="text-gray-400 hover:text-white transition"
+                  className="text-gray-300 hover:text-white transition"
                 >
                   Contact
                 </a>
@@ -50,7 +50,7 @@ const Footer = () => {
             <div className="flex space-x-4 flex-wrap">
               <a
                 href="https://www.facebook.com/shashank.musale.37?mibextid=ZbWKwL"
-                className="flex items-center text-gray-400 hover:text-white transition"
+                className="flex items-center text-gray-300 hover:text-white transition"
                 target="_blank"
               >
                 <i className="fab fa-facebook-f m-2"></i>
@@ -58,7 +58,7 @@ const Footer = () => {
               </a>
               <a
                 href="https://x.com/ShashankMusale1"
-                className="flex items-center text-gray-400 hover:text-white transition"
+                className="flex items-center text-gray-300 hover:text-white transition"
                 target="_blank"
               >
                 <i className="fab fa-twitter m-2"></i>
@@ -66,7 +66,7 @@ const Footer = () => {
               </a>
               <a
                 href="https://www.instagram.com/shashank_.0712?utm_source=qr&igsh=OGlnc2p2eHd0MDY5"
-                className="flex items-center text-gray-400 hover:text-white transition"
+                className="flex items-center text-gray-300 hover:text-white transition"
                 target="_blank"
               >
                 <i className="fab fa-instagram m-2"></i>
@@ -74,7 +74,7 @@ const Footer = () => {
               </a>
               <a
                 href="https://www.linkedin.com/in/shashank-musale-726b6021b"
-                className="flex items-center text-gray-400 hover:text-white transition"
+                className="flex items-center text-gray-300 hover:text-white transition"
                 target="_blank"
               >
                 <i className="fab fa-linkedin-in m-2"></i>
@@ -84,7 +84,7 @@ const Footer = () => {
           </div>
         </div>
 
-        <div className="mt-8 text-center text-gray-500">
+        <div className="mt-8 text-center text-gray-400">
           <p>
             &copy; {new Date().getFullYear()} Sha-Shank. All rights reserved.
           </p>
