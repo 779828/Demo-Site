0f908f5afbf2e9bf00eb9ed41d74b096210a7165
Shashank <shashankbmusale07@gmail.com>
2025-02-03 12:25:53 +0530

    logo upd

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,9 +1,28 @@
+import { useEffect, useState } from "react";
 import About from "./About";
 import Bento from "./Bento";
 import Carousel from "./Carousel";
 import Hero from "./Hero";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "../utils/firebase";
 
 const Main = () => {
+  const [token, setToken] = useState(null);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, async (user) => {
+      if (user) {
+        const token = await user.getIdToken(/* forceRefresh */ true);
+        setToken(token);
+        // console.log("Firebase ID Token:", token);
+      } else {
+        setToken(null);
+      }
+    });
+
+    return () => unsubscribe(); // Cleanup listener on unmount
+  }, [auth]);
+
   return (
     <div>
       <Hero />
