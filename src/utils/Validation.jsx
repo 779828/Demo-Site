4bc704b265591164a589ec9be5866a3bc5534633
Shashank <shashankbmusale07@gmail.com>
2025-02-05 16:47:12 +0530

    validation message change

--- src/utils/Validation.jsx
+++ src/utils/Validation.jsx
@@ -9,9 +9,18 @@ export const checkValidData = (name, email, password) => {
   const isPasswordValid =
     /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/.test(password);
 
-  if (!isNameValid) return toast.error("Name ID is not valid");
-  if (!isEmailValid) return toast.error("Email ID is not valid");
-  if (!isPasswordValid) return toast.error("Password is not valid");
+  if (!isNameValid)
+    return toast.error(
+      `Name is not valid! Please took reference to this John Doe`
+    );
+  if (!isEmailValid)
+    return toast.error(
+      `Email ID is not valid! Please took reference to this John@gmail.com`
+    );
+  if (!isPasswordValid)
+    return toast.error(
+      `Password is not valid! Please took reference to this Pass@1234`
+    );
 
   return null;
 };
